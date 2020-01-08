a1625d777cd02d5283a9b72a49c71a36fd2be24f
Anurag Bhandari <anurag.bhd@gmail.com>
2020-01-08 11:52:43 +0530

    Add a dynamic route page for Post
    - Add a new page for dynamic route /p/[id]
    - Refactor PostLink component
    - Refactor Index page
    - Remove old Post page

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router';
-import Layout from '../components/layout';
+import Layout from '../../components/layout';
 
 const Post = () => {
     const router = useRouter();
     return (
         <Layout>
-            <h1>{router.query.title}</h1>
+            <h1>{router.query.id}</h1>
             <p>This is the blog post content.</p>
         </Layout>
     );
