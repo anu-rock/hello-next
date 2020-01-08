3775da6a120702b311d847ec50c27e93b15c7a9c
Anurag Bhandari <anurag.bhd@gmail.com>
2020-01-08 11:35:13 +0530

    Refactor Index page as blog home page
    This is part of our experiment of working with dynamic content.
    
    - Add PostLink component to link to blog posts
    - Add a bunch of `PostLink`s to Index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Link from 'next/link';
 import Layout from '../components/layout';
+import PostLink from '../components/post-link';
 
 const Index = () => (
     <Layout>
-        <p>Hello Next.js</p>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink title="Hello Next.js" />
+            <PostLink title="Learn Next.js is awesome" />
+            <PostLink title="Deploy apps with Zeit" />
+        </ul>
     </Layout>
 );
 
